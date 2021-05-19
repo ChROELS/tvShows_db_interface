f614cef2adab9ec58c9162e166156a76168d71bb
ChristineR <57664986+ChROELS@users.noreply.github.com>
2021-05-19 17:30:38 +0200

    Maven built + DAO + template
    Starting the project

---
+++ tvShows_db/tvShows_db/src/main/java/eu/christineroels/tvShows_db/TvShowsDbApplication.java
@@ -0,0 +1,13 @@
+package eu.christineroels.tvShows_db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TvShowsDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TvShowsDbApplication.class, args);
+	}
+
+}
---
+++ tvShows_db/tvShows_db/src/test/java/eu/christineroels/tvShows_db/TvShowsDbApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.christineroels.tvShows_db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TvShowsDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
